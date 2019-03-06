596e6139997a710251c846c54163831ca13cd783
Owomugisha Birungi <brinnetbirungi@yahoo.com>
2019-03-05 23:24:51 -0500

    Fixed bugs on the front-end...changed the class of submit to id in the ajax request. Also fixed identifiers in the loops on the backend side.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,36 @@
 var friendArray = [
     {
-        "name":"John Doe",
-        "photo":"photo",
-        "scores":[]
-    }
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Alfred",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            2,
+            5,
+            1,
+            5,
+            5,
+            3,
+            1
+          ]
+      }
 ]
 
 module.exports = friendArray;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,14 +19,15 @@ app.post('/api/friends',function(req,res){
     var totalDifference = 10000;
     for(var j=0;j<friendsData.length;j++){
         var eachDifference = 0;
-        for(var i=o;j<userResponses.length;i++){
+        for(var i=0;i<userResponses.length;i++){
             var score = userResponses[i];
-            eachDifference += Math.abs(friendsData[i].score[i] -score);
-        }
+            eachDifference += Math.abs(friendsData[j].scores[i] - score);
+        };
+
         if(eachDifference<totalDifference){
             totalDifference = eachDifference;
-            matchName = friendsData[i].name;
-            matchPhoto = friendsData[i].photo;
+            matchName = friendsData[j].name;
+            matchPhoto = friendsData[j].photo;
         }
     }
     friendsData.push(userInput);
